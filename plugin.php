7586c0cfddd4409a6d8c100e94aab0f256749190
Chakrapani Gautam <cherrygot.dev@gmail.com>
2023-12-28 16:58:22 +0530

    docs(gutenberg): added a plugin header doc comment

---
+++ plugin.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Plugin Name:       GenZ Admin — Gutenberg
+ * Plugin URI:        https://cherrygot.me/plugins/genz-admin/
+ *
+ * @codingStandardsIgnoreStart
+ * Description:       This plugin revamps your entire WordPress admin interface to allow you to experience a modern, sleek, gutenberg-inspired admin UI. Tired of using the old WordPress way of doing things? Well, wait no more to give it a try!
+ * @codingStandardsIgnoreEnd
+ *
+ * Version:           0.0.0
+ * Requires at least: 6.4
+ * Requires PHP:      8.1
+ * Author:            Chakrapani Gautam
+ * Author URI:        https://cherrygot.me/
+ * License:           GPL v3 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain:       genz-admin
+ * Domain Path:       /languages
+ *
+ * @package cherrygot/genz_admin-gutenberg
+ */
+
+declare ( strict_types = 1 );
